8353e9d4dec9fe48fa998a91f1f736586d27b28a
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-04 12:10:10 +0200

    first commit

--- DiceGame/ContentView.swift
+++ DiceGame/ContentView.swift
@@ -8,19 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var showingPvPSheet = false
+    @State var showingPvCSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(red: 38/256, green: 108/256, blue: 59/256)
+                .ignoresSafeArea()
+            VStack {
+                
+                Button(action: {
+                    showingPvPSheet = true
+                }, label: {
+                    Text("PvP")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                })
+                .background(Color.red)
+                .cornerRadius(16)
+                
+                Button(action: {
+                    showingPvCSheet = false
+                }, label: {
+                    Text("PvC")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                })
+                .background(Color.red)
+                .cornerRadius(16)
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
+struct PvPView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 38/256, green: 108/256, blue: 59/256)
+                .ignoresSafeArea()
+            VStack {
+                
+            }
+            
+        }
+    }
+}
+
+struct PvCView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 38/256, green: 108/256, blue: 59/256)
+                .ignoresSafeArea()
+            VStack {
+                
+            }
+            
+        }
+    }
+
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
