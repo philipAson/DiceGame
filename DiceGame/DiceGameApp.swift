3aa6e3179868423882500c65c3372068dd1109a5
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-04-04 11:48:26 +0200

    Initial Commit

---
+++ DiceGame/DiceGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameApp.swift
+//  DiceGame
+//
+//  Created by Philip Andersson on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
